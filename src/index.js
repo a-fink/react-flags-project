aa3e99d4b61ca2dc9d469cbacd7e40e51c0f8cf9
Aubrey <aubreyfinkelstein@gmail.com>
2022-10-10 18:59:53 -0700

    react router and correction to react 18 rendering set up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
+import {BrowerRoute, BrowserRouter} from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
+// wrap entire App in BrowserRouter component to allow browser side routing
+const BrowserRoot = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
+
+// set up root component in the new style with createRoot for React 18
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRoot />
+  </React.StrictMode>
 );
