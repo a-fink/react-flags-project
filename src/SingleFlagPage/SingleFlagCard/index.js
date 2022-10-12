f4eeb1c9e46f80be6c3124e16a0effe34e21cecc
Aubrey <aubreyfinkelstein@gmail.com>
2022-10-11 18:02:36 -0700

    single flag card image & inforamtion functionality working, and styling in progress. need to make bordering country buttons functionality

--- src/SingleFlagPage/SingleFlagCard/index.js
+++ src/SingleFlagPage/SingleFlagCard/index.js
@@ -1,64 +1,68 @@
 import './SingleFlagCard.css';
 
-function SingleFlagCard(){
-    // ** TO DO ** logic to make this render each flag passed in - using flag placeholder for now
-    const flagURL = "https://m.media-amazon.com/images/I/41TeyWaQeQL._AC_SX466_.jpg";
-    const flagText = "american flag";
-    const countryName = "United States of America";
-    const dataPlaceholder = "placeholder";
+// input - a country found from the API by the parent component
+// output - a JSX element rendering the country flag and information
+function SingleFlagCard({country}){
+    // get a string of the currencies from the country object
+    let currencyArray = [];
+    for (const [key, value] of Object.entries(country.currencies)){
+        currencyArray.push(value.name);
+    }
+    const currencies = currencyArray.join(', ');
+
+    // get a string of the languages from the country object
+    let languagesArray = [];
+    for(const [key, value] of Object.entries(country.languages)){
+        languagesArray.push(value);
+    }
+    const languages = languagesArray.join(', ');
 
     return (
         <div className='single-flag-card'>
-            <img src={flagURL} alt={flagText} className="single-flag_image"></img>
+            <img src={country.flags.svg} alt={`Flag of ${country.name.common}`} className="single-flag_image"></img>
             <div className='single-flag_info-section'>
-                <h2 className='single-flag_heading'>{countryName}</h2>
+                <h2 className='single-flag_heading'>{country.name.common}</h2>
                 <div className='single-flag_info'>
-                    <p className="single-flag_category">
-                        Native Name:
-                        <span className="single-flag_data">
-                            {` ${dataPlaceholder}`}
-                        </span>
-                    </p>
                     <p className="single-flag_category">
                         Population:
                         <span className="single-flag_data">
-                            {` ${dataPlaceholder}`}
+                            {` ${country.population.toLocaleString('en-US')}`}
                         </span>
                     </p>
                     <p className="single-flag_category">
                         Region:
                         <span className="single-flag_data">
-                            {` ${dataPlaceholder}`}
+                            {` ${country.region}`}
                         </span>
                     </p>
                     <p className="single-flag_category">
                         Sub Region:
                         <span className="single-flag_data">
-                            {` ${dataPlaceholder}`}
+                            {` ${country.subregion}`}
                         </span>
                     </p>
                     <p className="single-flag_category">
                         Capital:
                         <span className="single-flag_data">
-                            {` ${dataPlaceholder}`}
+                            {` ${country.capital}`}
                         </span>
                     </p>
                     <p className="single-flag_category">
                         Top Level Domain:
                         <span className="single-flag_data">
-                            {` ${dataPlaceholder}`}
+                            {` ${country.tld}`}
                         </span>
                     </p>
                     <p className="single-flag_category">
                         Currencies:
                         <span className="single-flag_data">
-                            {` ${dataPlaceholder}`}
+                            {` ${currencies}`}
                         </span>
                     </p>
                     <p className="single-flag_category">
                         Languages:
                         <span className="single-flag_data">
-                            {` ${dataPlaceholder}`}
+                            {` ${languages}`}
                         </span>
                     </p>
                 </div>
--- src/SingleFlagPage/SingleFlagContainer/index.js
+++ src/SingleFlagPage/SingleFlagContainer/index.js
@@ -1,16 +1,67 @@
 import BackButton from "../../BackButton";
 import SingleFlagCard from "../SingleFlagCard";
 import './SingleFlagContainer.css';
+import { useParams, Redirect } from 'react-router-dom';
+import { useState, useEffect} from 'react';
 
 function SingleFlagContainer() {
-    return (
-        <div className="single-flag-container">
-            <div className="back-button-positioner">
-                <BackButton />
+    const {code} = useParams();
+
+    // useState to get values for whether data has loaded, the country, and whether there was an error
+    // starting states are no errors, loaded is false, and countries is an empty object
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [country, setCountry] = useState({});
+
+    // useEffect to search based on country code gotten from the parameters
+    // this function component should only get re-run/re-rendered each time a new url happens
+    // from it's parent component or if someone links to the page, but just in case make it run anytime code changes
+    // even when searching for one country by code, API returns an array, so use 1st element in result
+    useEffect(() => {
+        fetch(`https://restcountries.com/v3.1/alpha/${code}`)
+        .then(res => res.json())
+        .then((result) => {
+            setIsLoaded(true);
+            setCountry(result[0]);
+        })
+        .catch((err) => {
+            setIsLoaded(true);
+            setError(err);
+        })
+    }, [code]);
+
+    // if an error happened redirect to the error page
+    // when a request fails the response object will have a message key
+    // code will be valid code if user came from a country card on the home page
+    // but if they type url/try to link directly there could be invalid code
+    // for this case, also send them to the error page
+    if(error || country.message){
+        return <Redirect to='/error' />
+    }
+    // if isLoaded is false display a loading message
+    else if(!isLoaded){
+        return (
+            <div className="single-flag-container">
+                <div className="back-button-positioner">
+                    <BackButton />
+                </div>
+                <div className="single-flag_loading">
+                    <p className="single-flag_message">Loading...</p>
+                </div>
             </div>
-            <SingleFlagCard />
-        </div>
-    );
+        );
+    }
+    // finally if data found send the country to the single flag card to render
+    else{
+        return (
+            <div className="single-flag-container">
+                <div className="back-button-positioner">
+                    <BackButton />
+                </div>
+                <SingleFlagCard country={country} />
+            </div>
+        );
+    }
 }
 
 export default SingleFlagContainer;
