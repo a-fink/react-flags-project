93301954c2e94b7e837ff8a416cbb5fb19a364aa
Aubrey <aubreyfinkelstein@gmail.com>
2022-10-12 12:10:43 -0700

    search bar functionality working

--- src/HomePage/HomePageContainer/index.js
+++ src/HomePage/HomePageContainer/index.js
@@ -7,10 +7,11 @@ import { useState } from 'react';
 // inputs - none
 // outputs - component that renders all the home page elements
 function HomePageContainer(){
-    // useState to set a search string, starts out as 'all'
+    // useState to set a search string, starts out as 'all', and a search display value for the search bar, starts as ''
     // search string will get updated by search bar or filter selector
     // then passed to all flags container to search / render the results
     const [searchString, setSearchString] = useState('all');
+    const [searchDisplay, setSearchDisplay] = useState('');
 
     // click handler that will be passed to the filter selector which renders the drop down menu
     // the correct search string we need is stored as the id of each item in the drop down menu
@@ -20,10 +21,30 @@ function HomePageContainer(){
         setSearchString(event.target.id);
     }
 
+    // change handler that will be passed to the search bar
+    // when the value of the search bar changes we want to update the search string
+    // this will update what displays in the search bar and re-render the all flags container with the new search
+    const changeHandler = (event) => {
+        // console.log(event.target.value)
+
+        // set the search display to the new value of the search bar
+        setSearchDisplay(event.target.value);
+
+        // if the search bar's value has gone back to '' we want to search for all
+        if(event.target.value === ''){
+            setSearchString('all');
+        }
+        // otherwise set search string to the correct string to look for a country by name
+        else{
+            setSearchString(`name/${event.target.value}`);
+        }
+    }
+
+
     return (
         <div className="home-page-container">
             <div className="search-container">
-                <SearchBar />
+                <SearchBar searchDisplay={searchDisplay} changeHandler={changeHandler}/>
                 <FilterSelector clickHandler={clickHandler}/>
             </div>
             <AllFlagsContainer searchString={searchString}/>
--- src/HomePage/SearchBar/index.js
+++ src/HomePage/SearchBar/index.js
@@ -1,12 +1,23 @@
 import './SearchBar.css';
 
-function SearchBar() {
+// inputs - event handler and search string from parent
+// return - jsx element rendering search bar with search string
+function SearchBar({changeHandler, searchDisplay}) {
+    // if the search string is the default value of all then we want search bar text to ask for input
+    // if the search string is something else then we want to display that
+
     return (
         <div className="search-bar-container">
             <div className="search-icon">
                 <ion-icon name="search-outline"></ion-icon>
             </div>
-            <input type="text" value="Search for a country by name..." className="search-bar"></input>
+            <input
+                type="text"
+                placeholder='Search by common or native name...'
+                value={searchDisplay}
+                onChange={changeHandler}
+                className="search-bar"
+            />
         </div>
     );
 }
