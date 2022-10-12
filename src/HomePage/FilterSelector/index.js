2a8ff6c99aef07f9c2f9b9e7e4d7efa2fa1db387
Aubrey <aubreyfinkelstein@gmail.com>
2022-10-12 11:14:09 -0700

    dropdown menu refactored, and appear/disappear functionality working

--- src/HomePage/FilterSelector/index.js
+++ src/HomePage/FilterSelector/index.js
@@ -1,15 +1,33 @@
 import './FilterSelector.css';
+import {useState} from 'react';
 
+// inputs - event handler from parent
+// return - component that renders top level button, then depending on state the sub-options with the event handler
 function FilterSelector() {
+    // have state for whether the drop down portion should be visible - starts off
+    const [showDropDown, setShowDropDown] = useState(false);
+
+    const clickHandler = () => {
+        setShowDropDown((prev) => !prev);
+    }
+
     return (
-        <select name='region' className="region-selector">
-            <option value="">Filter By Region</option>
-            <option value="Africa">Africa</option>
-            <option value="Americas">Americas</option>
-            <option value="Asia">Asia</option>
-            <option value="Europe">Europe</option>
-            <option value="Oceania">Oceania</option>
-        </select>
+        <div className='filter-selector-container'>
+            <button onClick={clickHandler} className="region-selector">
+                Fliter by region
+                <ion-icon name="chevron-down-outline"></ion-icon>
+            </button>
+            {!showDropDown ? null :
+                <div className='drop-down-menu'>
+                    <button className='drop-down-button' id='all'>All</button>
+                    <button className='drop-down-button' id='region/africa'>Africa</button>
+                    <button className='drop-down-button' id='region/americas'>Americas</button>
+                    <button className='drop-down-button' id='region/asia'>Asia</button>
+                    <button className='drop-down-button' id='region/europe'>Europe</button>
+                    <button className='drop-down-button' id='region/oceania'>Oceania</button>
+                </div>
+            }
+        </div>
     )
 }
 
